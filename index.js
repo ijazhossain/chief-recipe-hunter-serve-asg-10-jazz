10b3f4bb8a0c00ea62c4e80761e0072ea89ee575
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-02 01:50:00 +0600

    data.json added

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors())
+const data = require('./data.json')
 
 app.get('/', (req, res) => {
     res.send('Chef recipe hunter server is running')
+})
+app.get('/home', (req, res) => {
+    res.send(data)
 })
 app.listen(port, () => {
     console.log('Chef recipe hunter is listening on port', port);
