382a255e9f618133900a67eb2b265a05ccd5337b
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-03 00:23:18 +0600

    data send to recipe details page

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     res.send(data)
 })
+app.get('/details', (req, res) => {
+    res.send(data)
+})
+app.get('/details/:chefId', (req, res) => {
+    const id = req.params.chefId;
+    console.log(id);
+    const chefData = data.find(chef => chef._id == id)
+    res.send(chefData)
+})
 app.listen(port, () => {
     console.log('Chef recipe hunter is listening on port', port);
 })
